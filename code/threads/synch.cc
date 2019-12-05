1bbd00b3164f62ed77cb2ffd37caba93560189fe
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-12-05 17:29:50 -0500

    filesysBackbone

--- code/threads/synch.cc
+++ code/threads/synch.cc
@@ -203,6 +203,11 @@ void Lock::Release()
     semaphore->V();
 }
 
+bool Lock:: IsHeldByCurrentThread() 
+{ 
+    return lockHolder == kernel->currentThread; 
+}
+
 //----------------------------------------------------------------------
 // Condition::Condition
 // 	Initialize a condition variable, so that it can be 
