0456823ee25566f3f96555a8a060df4993347534
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-12-06 02:59:14 -0500

    filesysBackbone

--- code/filesys/openfile.cc
+++ code/filesys/openfile.cc
@@ -219,7 +219,7 @@ OpenFile::WriteAt(char *from, int numBytes, int position)
     if (!lastAligned && ((firstSector != lastSector) || firstAligned))
         ReadAt(&buf[(lastSector - firstSector) * SectorSize], 
 				SectorSize, lastSector * SectorSize);	
-
+   printf("here3\n");
 // copy in the bytes we want to change 
     bcopy(from, &buf[position - (firstSector * SectorSize)], numBytes);
 
