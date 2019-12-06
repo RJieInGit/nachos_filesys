2e44e75637aa57027465bbda1428f3487ff36c3b
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-12-06 04:23:35 -0500

    debug

--- code/filesys/filesys.cc
+++ code/filesys/filesys.cc
@@ -243,6 +243,7 @@ FileSystem::Create(char *name, int initialSize, int wdSector)
 	    if (!hdr->Allocate(freeMap, initialSize))
             	success = FALSE;	// no space on disk for data
 	    else {	
+            directory->Print();
 	    	success = TRUE;
 		// everthing worked, flush all changes back to disk
     	    	hdr->WriteBack(sector); 		
